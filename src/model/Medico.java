6dacd6740a71716b62555490b7f178295881be39
Kevinbarbosa <kevin6252@outlook.com>
2024-11-28 10:52:45 -0300

    teste de commit

--- src/model/Medico.java
+++ src/model/Medico.java
@@ -1,17 +1,27 @@
 package model;
 
-public class Medico extends Usuario {
+public class Medico extends UsuarioModel {
+    private String nome;
     private String especialidade;
     private String crm;
-    public Medico(String nome, String telefone, String email, String especialidade, String crm) {
-        super(nome, email, telefone);
+    private String telefone;
+    public Medico(String nome,String email,String senha,String especialidade, String crm, String telefone) {
+        super( email, senha);
+        this.nome = nome;
         this.especialidade = especialidade;
         this.crm = crm;
+        this.telefone = telefone;
+    }
+    public String getNome() {
+        return nome;
     }
     public String getEspecialidade() {
         return especialidade;
     }
     public String getCrm() {
         return crm;
+    }
+    public String getTelefone() {
+        return telefone;
     }
 } 
--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -1,22 +1,30 @@
 package model;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Paciente extends Usuario{
+public class Paciente extends UsuarioModel{
+    private String nome;
     private String cpf;
     private String endereco;
-    private String dataNascimento;
+    private String telefone;
+    private Date dataNascimento;
     private List<Historico> historicoMedico = new ArrayList<>();
     private Medicamento medicamentos;
     private String dispositivos;
-    public Paciente(String nome, String cpf, String telefone, String email, String endereco, String dataNascimento, List<Historico> historicoMedico, Medicamento medicamentos, String dispositivos) {
-        super(nome, email, telefone);
+    public Paciente(String nome, String cpf, String telefone, String email, String senha,String endereco,Date dataNascimento, List<Historico> historicoMedico, Medicamento medicamentos, String dispositivos) {
+        super(email, senha);
+        this.nome = nome;
         this.cpf = cpf;
         this.endereco = endereco;
+        this.telefone = telefone;
         this.dataNascimento = dataNascimento;
         this.historicoMedico = (historicoMedico != null) ? historicoMedico : new ArrayList<>();
         this.medicamentos = medicamentos;
         this.dispositivos = dispositivos;
+    }
+    public String getNome() {
+        return nome;
     }
     public String getCpf() {
         return cpf;
@@ -24,7 +32,10 @@ public class Paciente extends Usuario{
     public String getEndereco() {
         return endereco;
     }
-    public String getDataNascimento() {
+    public String getTelefone() {
+        return telefone;
+    }
+    public Date getDataNascimento() {
         return dataNascimento;
     }
     public List<Historico> getHistoricoMedico() {
