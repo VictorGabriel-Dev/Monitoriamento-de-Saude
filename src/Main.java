025eef9a3a57bf64c8dcb1df571f063646245a4a
Kevinbarbosa <kevin6252@outlook.com>
2024-12-05 16:23:29 -0300

    ....

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import controller.AppController;
+public class Main {
+    public static void main(String[] args) {
+        AppController appController = new AppController();
+        appController.run();
+
+    }
+}
