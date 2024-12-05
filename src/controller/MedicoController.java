80551aa1a408ad1c80aef8bd9fcdbabde93301ff
Kevinbarbosa <kevin6252@outlook.com>
2024-12-05 19:14:05 -0300

    .....

--- src/controller/MedicoController.java
+++ src/controller/MedicoController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MedicoController {
+public class MedicoController extends BaseController<Medico> {
     private Medico medico;
     private MedicoView medicoView;
     private ConsultaController consultaController;
@@ -22,16 +22,70 @@ public class MedicoController {
         this.dispositivoController = new DispositivoController();
 
     }
+
     public void dadosMedico() {
         medicoView.exibirDados(medico);
     }
+    @Override
+public void alterarDados(Medico medico) {
+    int opcao;
+    do {
+        opcao = medicoView.selecionarQualAlterar();
+        String dadoAnterior = null;
+        boolean confirmacao = false;
+
+        switch (opcao) {
+            case 1:
+                dadoAnterior = medico.getNome();
+                medico.setNome(solicitarEntrada("Digite o novo nome: "));
+                break;
+            case 2:
+                dadoAnterior = medico.getEspecialidade();
+                medico.setEspecialidade(solicitarEntrada("Digite a nova especialidade: "));
+                break;
+            case 3:
+                dadoAnterior = medico.getCrm();
+                medico.setCrm(solicitarEntrada("Digite o novo CRM: "));
+                break;
+            case 4:
+                dadoAnterior = medico.getTelefone();
+                medico.setTelefone(solicitarEntrada("Digite o novo telefone: "));
+                break;
+            case 5:
+                dadoAnterior = medico.getEmail();
+                medico.setEmail(solicitarEntrada("Digite o novo e-mail: "));
+                break;
+            case 6:
+                return; // Sai do menu
+            default:
+                System.out.println("Opção inválida.");
+                continue;
+        }
+
+        // Confirmação da alteração
+        confirmacao = confirmarAlteracao();
+        if (!confirmacao) {
+            // Reverte a alteração caso o usuário não confirme
+            if (opcao == 1) medico.setNome(dadoAnterior);
+            if (opcao == 2) medico.setEspecialidade(dadoAnterior);
+            if (opcao == 3) medico.setCrm(dadoAnterior);
+            if (opcao == 4) medico.setTelefone(dadoAnterior);
+            if (opcao == 5) medico.setEmail(dadoAnterior);
+            exibirMensagem("Alteração cancelada.");
+        } else {
+            exibirMensagem("Alteração confirmada com sucesso!");
+        }
+    } while (opcao != 6);
+}
+
+
     public void alterarEVoltar() {
         int opcao;
         do {
             opcao = medicoView.opcoesAlterarEVoltar();
             switch (opcao) {
                 case 1:
-                    medicoView.alterarDados(medico);
+                    alterarDados(medico);
                     break;
                 case 2:
                     return;
@@ -40,6 +94,7 @@ public class MedicoController {
             }
         } while (opcao != 2);
     }
+
     public void menu() {
         int opcao;
         do {
@@ -53,7 +108,7 @@ public class MedicoController {
                     System.out.println("Voltando ao menu principal.");
                     break;
                 case 3:
-                consultarAgendamentos();
+                    consultarAgendamentos();
                     break;
                 case 4:
                     dispositivoController.menu();
@@ -64,7 +119,13 @@ public class MedicoController {
                     controller.alertaMenu();
                     break;
                 case 6:
-                    MonitoramentoController monitoramentoController = new MonitoramentoController(pacientes); // Certifique-se de que a lista de pacientes foi inicializada
+                    MonitoramentoController monitoramentoController = new MonitoramentoController(pacientes); // Certifique-se
+                                                                                                              // de que
+                                                                                                              // a lista
+                                                                                                              // de
+                                                                                                              // pacientes
+                                                                                                              // foi
+                                                                                                              // inicializada
                     monitoramentoController.menuMonitoramento();
                     break;
                 case 7:
@@ -74,25 +135,26 @@ public class MedicoController {
             }
         } while (opcao != 7);
     }
-        public void consultarAgendamentos() {
+
+    public void consultarAgendamentos() {
         // Exibe todas as consultas agendadas para o médico
         if (medico.getConsultas() != null && !medico.getConsultas().isEmpty()) {
             System.out.println("Consultas agendadas para o médico " + medico.getNome() + ":");
             for (Consulta consulta : medico.getConsultas()) {
-                System.out.println("Paciente: " + consulta.getPaciente().getNome() + ", Data: " + consulta.getDataConsulta());
+                System.out.println(
+                        "Paciente: " + consulta.getPaciente().getNome() + ", Data: " + consulta.getDataConsulta());
             }
         } else {
             System.out.println("Não há consultas agendadas.");
         }
     }
 
-
-    //metodo lista de paciente
+    // metodo lista de paciente
     public static Paciente selecionarPaciente(List<Paciente> pacientes) {
         Scanner sc = new Scanner(System.in);
         System.out.println("\n--- Lista de Pacientes ---");
-        for(int i=0; i < pacientes.size(); i++){
-            System.out.println((i+1)+". "+ pacientes.get(i).getNome());
+        for (int i = 0; i < pacientes.size(); i++) {
+            System.out.println((i + 1) + ". " + pacientes.get(i).getNome());
         }
 
         System.out.println("Selecione o númerio do paciente (ou 0 para voltar)");
@@ -100,16 +162,16 @@ public class MedicoController {
         sc.nextLine();
 
         // Voltar ao menu principal
-        if (escolhaPaciente == 0){
-            return null;//escolha voltar
+        if (escolhaPaciente == 0) {
+            return null;// escolha voltar
         }
-        //validando escolha
-        if (escolhaPaciente < 1 || escolhaPaciente > pacientes.size()){
+        // validando escolha
+        if (escolhaPaciente < 1 || escolhaPaciente > pacientes.size()) {
             System.out.println("Opção inválida! Tente novamente.");
             return null;
         }
 
         // Selecione o paciente com base na escolha
-        return pacientes.get(escolhaPaciente -1);
+        return pacientes.get(escolhaPaciente - 1);
     }
 }
--- src/controller/PacienteController.java
+++ src/controller/PacienteController.java
@@ -3,8 +3,9 @@ package controller;
 import java.util.Scanner;
 import model.Paciente;
 import view.PacienteView;
+import java.time.LocalDate;
 
-public class PacienteController {
+public class PacienteController extends BaseController<Paciente> {
     private Paciente paciente;
     private ConsultaController consultaController;
     private PacienteView pacienteView;
@@ -21,13 +22,65 @@ public class PacienteController {
         pacienteView.exibirDados(paciente);
     }
 
+    @Override
+    public void alterarDados(Paciente paciente) {
+        int opcao;
+        do {
+            opcao = pacienteView.selecionarQualAlterar();
+            String dadoAnterior = null;
+            boolean confirmacao = false;
+            switch (opcao) {
+                case 1:
+                    dadoAnterior = paciente.getNome();
+                    paciente.setNome(solicitarEntrada("Digite o novo nome: "));
+                    break;
+                case 2:
+                    dadoAnterior = paciente.getCpf();
+                    paciente.setCpf(solicitarEntrada("Digite o novo CPF: "));
+                    break;
+                case 3:
+                    dadoAnterior = paciente.getDataNascimento().toString();
+                    paciente.setDataNascimento(LocalDate.parse(solicitarEntrada( "Digite a nova data de nascimento (AAAA-MM-DD): ")));
+                    break;
+                case 4:
+                    dadoAnterior = paciente.getEndereco();
+                    paciente.setEndereco(solicitarEntrada("Digite o novo endereço: "));
+                case 5:
+                    dadoAnterior = paciente.getTelefone();
+                    paciente.setTelefone(solicitarEntrada("Digite o novo telefone: "));
+                    break;
+                case 6:
+                    dadoAnterior = paciente.getEmail();
+                    paciente.setEmail(solicitarEntrada("Digite o novo e-mail: "));
+                    break;
+                case 7:
+                    return; // Sai do menu
+                    default:
+                    System.out.println("Opção inválida.");
+                    continue;
+            }
+        confirmacao = confirmarAlteracao();
+        if (!confirmacao) {
+            if(opcao == 1) paciente.setNome(dadoAnterior);
+            if(opcao == 2) paciente.setCpf(dadoAnterior);
+            if(opcao == 3) paciente.setDataNascimento(LocalDate.parse(dadoAnterior));
+            if(opcao == 4) paciente.setEndereco(dadoAnterior);
+            if(opcao == 5) paciente.setTelefone(dadoAnterior);
+            if(opcao == 6) paciente.setEmail(dadoAnterior);
+            exibirMensagem("Alteração cancelada.");
+            }else{
+                exibirMensagem("Alteração realizada com sucesso!");
+            }
+        }while (opcao != 7);
+    } 
+
     public void alterarEVoltar() {
         int opcao;
         do {
             opcao = pacienteView.opcoesAlterarEVoltar();
             switch (opcao) {
                 case 1:
-                    pacienteView.alterarDados(paciente);
+                    alterarDados(paciente);
                     break;
                 case 2:
                     return;
