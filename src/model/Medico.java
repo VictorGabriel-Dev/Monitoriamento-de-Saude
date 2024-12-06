a0f71a58da57ce7c11acc1593a71adfda1af9e3d
Victor_Alves <164104733+VictorGabriel-Dev@users.noreply.github.com>
2024-12-06 11:08:13 -0300

    entrega

---
+++ src/model/Medico.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.util.List;
+
+public class Medico extends UsuarioModel {
+    private String nome;
+    private String especialidade;
+    private String crm;
+    private String telefone;
+    private List<Consulta> consultas;
+
+    public Medico(String nome, String email, String senha, String especialidade, String crm, String telefone) {
+        super(email, senha);
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.crm = crm;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public List<Consulta> getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(List<Consulta> consultas) {
+        this.consultas = consultas;
+    }
+
+    @Override
+    public String toString() {
+        return "\n********** Dados do Paciente **********\n" +
+                "Nome: " + nome + "\n" +
+                "Especialidade: " + especialidade + "\n" +
+                "CRM: " + crm + "\n" +
+                "Telefone: " + telefone + "\n" +
+                "Email: " + getEmail() + "\n";
+    }
+}
---
+++ src/view/MonitoramentoView.java
@@ -0,0 +1,46 @@
+package view;
+
+import model.Medicamento;
+import model.Paciente;
+import java.util.Scanner;
+
+public class MonitoramentoView {
+    private Scanner sc = new Scanner(System.in);
+    // Menu monitoramento
+    public int monitoramentoPaciente(Paciente paciente) {
+        System.out.println("\n--- Monitoramento de " + paciente.getNome() + " ---");
+        System.out.println("1. Visualizar dados de monitoração");//testanto esse metodo
+        System.out.println("2. Gerar alerta do paciente");
+        System.out.println("3. Analisar");//historico de consulta, sintomas, medicamentos
+        System.out.println("0. Voltar");
+        System.out.println("Escolha uma opção: ");
+        int escolha = sc.nextInt();
+        sc.nextLine();
+        return escolha;
+    }
+
+    // View do menu monitoramento
+    public int exibirMenuMonitoramento() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Menu Monitoramento ---");
+        System.out.println("1. Dados de monitoração");
+        System.out.println("2. Análise");
+        System.out.println("3. Sair");
+        System.out.print("Escolha uma opção: ");
+        return sc.nextInt();
+    }
+
+    public void exibirMensagem(String mensagem) {
+        System.out.println(mensagem);
+    }
+    //Medicamentos
+    public void dadosMedicamento(Medicamento medicamento) {
+        System.out.println("\n--- Dados do Medicamento ---");
+        System.out.println("Nome: " + medicamento.getNome());
+        System.out.println("Dosagem: " + medicamento.getDosagem());
+        System.out.println("Frequência: " + medicamento.getFrequencia());
+        System.out.println("Descrição: " + medicamento.getDescricao());
+        System.out.println("Médico responsavel: " + medicamento.getMedico());
+        System.out.println("Data da prescrição: " + medicamento.getDataPrescricao());
+    }
+}
